975c4d82a41d9fe5ade84b4d1e84a0bc1b660d62
mukeshlokare <mukesh.lokare1@gmail.com>
2024-01-01 00:52:31 -0800

    m1

---
+++ gitestdemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gitestdemo
+//
+//  Created by Mukesh Lokare on 01/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gitestdemo/gitestdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitestdemoApp.swift
+//  gitestdemo
+//
+//  Created by Mukesh Lokare on 01/01/24.
+//
+
+import SwiftUI
+
+@main
+struct gitestdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
